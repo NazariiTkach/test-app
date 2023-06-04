60c78606c401313483599bb547f958667c41a950
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-06-04 12:11:21 +0200

    commit3

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
